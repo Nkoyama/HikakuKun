962207cfed8f037cf27750d908df01a6522fdd7d
NKoyama <n.koyama24@gmail.com>
2020-07-26 18:11:23 +0900

    SnapKit追加

--- HikakuKun/HikakuKun/scenes/ViewController.swift
+++ HikakuKun/HikakuKun/scenes/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import SnapKit
+import RealmSwift
 
 class ViewController: UIViewController {
+
+	// データ表示用TabelView
+	var hikakuListTable	= UITableView()
+	var allGroupNames: [String] = []
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -17,8 +23,29 @@ class ViewController: UIViewController {
 
 		// background color
 		self.view.backgroundColor = UIColor.white
+
+		// 表示用データ取得
+		let realm = try! Realm()
+		let allItems = realm.objects(HikakuItemRealm.self)
+		for item in allItems {
+			allGroupNames.append(item.groupName)
+		}
+
+		// 取得したデータ表示
+		hikakuListTable.frame = view.bounds
+		hikakuListTable.dataSource = self
+		view.addSubview(hikakuListTable)
 	}
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return allGroupNames.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell()
+		cell.textLabel?.text = allGroupNames[indexPath.row]
+		return cell
+	}
+}
