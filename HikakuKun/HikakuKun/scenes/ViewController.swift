3da34fee790a77284bed23b4e915d8ea2d0cf7fc
NKoyama <n.koyama24@gmail.com>
2020-07-26 18:25:59 +0900

    登録データ表示部分

--- HikakuKun/HikakuKun/scenes/ViewController.swift
+++ HikakuKun/HikakuKun/scenes/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
 
 	// データ表示用TabelView
 	var hikakuListTable	= UITableView()
-	var allGroupNames: [String] = []
+	var displayList: [String] = []
+
+	let addNewHikakuBtn		= UIButton()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -26,26 +28,45 @@ class ViewController: UIViewController {
 
 		// 表示用データ取得
 		let realm = try! Realm()
-		let allItems = realm.objects(HikakuItemRealm.self)
+		let allItems	= realm.objects(HikakuItemRealm.self)
 		for item in allItems {
-			allGroupNames.append(item.groupName)
+			var displayText = item.groupName
+			let contents = realm.objects(HikakuContentsRealm.self).filter("groupID="+String(item.groupId))
+			var i = 0
+			for content in contents {
+				if( i == 0 ) {
+					displayText = displayText + "  : " + content.name
+				} else {
+					displayText = displayText + ", " + content.name
+				}
+				i += 1
+			}
+			displayList.append(displayText)
 		}
 
 		// 取得したデータ表示
-		hikakuListTable.frame = view.bounds
-		hikakuListTable.dataSource = self
-		view.addSubview(hikakuListTable)
+		self.hikakuListTable.dataSource = self
+		view.addSubview(self.hikakuListTable)
+		self.hikakuListTable.snp.makeConstraints { make in
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(0)
+			make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).inset(0)
+			make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(0)
+			make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).inset(100)
+		}
+
+		// add new hikaku button
+		
 	}
 }
 
 extension ViewController: UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return allGroupNames.count
+		return displayList.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = UITableViewCell()
-		cell.textLabel?.text = allGroupNames[indexPath.row]
+		cell.textLabel?.text = displayList[indexPath.row]
 		return cell
 	}
 }
