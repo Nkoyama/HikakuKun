fba931bebfb86d170aff767b8a37104571800988
NKoyama <n.koyama24@gmail.com>
2020-07-26 17:27:39 +0900

    Realm用object作成

---
+++ HikakuKun/HikakuKun/RealmClasses/HikakuContentsRealm.swift
@@ -0,0 +1,27 @@
+//
+//  HikakuRealm.swift
+//  HikakuKun
+//
+//  Created by Nozomi Koyama on 2020/07/26.
+//  Copyright © 2020 Nozomi Koyama. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class HikakuContentsRealm: Object {
+	dynamic var groupId		: Int		= 0		// 比較グループID
+	dynamic var id			: Int		= 0		// 比較グループ内ID
+	dynamic var name		: String	= ""	// 比較対象名前
+	dynamic var content1	: String	= ""	// 比較項目①内容
+	dynamic var content2	: String	= ""	// 比較項目②内容
+	dynamic var content3	: String	= ""	// 比較項目③内容
+	dynamic var content4	: String	= ""	// 比較項目④内容
+	dynamic var content5	: String	= ""	// 比較項目⑤内容
+	dynamic var content6	: String	= ""	// 比較項目⑥内容
+	dynamic var content7	: String	= ""	// 比較項目⑦内容
+	dynamic var content8	: String	= ""	// 比較項目⑧内容
+	dynamic var content9	: String	= ""	// 比較項目⑨内容
+	dynamic var content10	: String	= ""	// 比較項目⑩内容
+	dynamic var memo		: String	= ""	// memo
+}
---
+++ HikakuKun/HikakuKun/RealmClasses/HikakuItemRealm.swift
@@ -0,0 +1,25 @@
+//
+//  HikakuItemRealm.swift
+//  HikakuKun
+//
+//  Created by Nozomi Koyama on 2020/07/26.
+//  Copyright © 2020 Nozomi Koyama. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class HikakuItemRealm: Object {
+	dynamic var groupId		: Int		= 0		// 比較グループID
+	dynamic var groupName	: String	= ""	// 比較グループ名
+	dynamic var item1		: String	= ""	// 比較項目①
+	dynamic var item2		: String	= ""	// 比較項目②
+	dynamic var item3		: String	= ""	// 比較項目③
+	dynamic var item4		: String	= ""	// 比較項目④
+	dynamic var item5		: String	= ""	// 比較項目⑤
+	dynamic var item6		: String	= ""	// 比較項目⑥
+	dynamic var item7		: String	= ""	// 比較項目⑦
+	dynamic var item8		: String	= ""	// 比較項目⑧
+	dynamic var item9		: String	= ""	// 比較項目⑨
+	dynamic var item10		: String	= ""	// 比較項目⑩
+}
