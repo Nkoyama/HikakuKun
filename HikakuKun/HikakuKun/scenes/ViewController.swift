8c8334909906bb2494b07ea496e6640774784093
NKoyama <n.koyama24@gmail.com>
2020-07-26 16:39:44 +0900

    Realm導入

---
+++ HikakuKun/HikakuKun/scenes/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HikakuKun
+//
+//  Created by Nozomi Koyama on 2020/07/26.
+//  Copyright © 2020 Nozomi Koyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		title = "比較リスト"
+
+		// background color
+		self.view.backgroundColor = UIColor.white
+	}
+
+
+}
+
---
+++ HikakuKun/Pods/Target Support Files/Pods-HikakuKun/Pods-HikakuKun-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HikakuKun : NSObject
+@end
+@implementation PodsDummy_Pods_HikakuKun
+@end
