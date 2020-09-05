037fb1c32c8ea2df94849ff6d59d8d7ad28fc184
Nkoyama <n.koyama24@gmail.com>
2020-09-05 13:55:35 +0900

    使い方画面 閉じるボタン設置

--- HikakuKun/HikakuKun/Scenes/HowToUseVC.swift
+++ HikakuKun/HikakuKun/Scenes/HowToUseVC.swift
@@ -11,8 +11,30 @@ import SnapKit
 
 class HowToUseVC: UIViewController {
 	
+	let closeBtn		= UIButton()
+
 	override func viewDidLoad() {
 		// background color
 		self.view.backgroundColor = UIColor.white
+
+		// close button
+		self.closeBtn.setTitle("× close", for: .normal)
+		self.closeBtn.setTitleColor(UIColor.blue, for: .normal)
+		self.closeBtn.backgroundColor = UIColor.clear
+		self.closeBtn.layer.borderColor = UIColor.clear.cgColor
+		self.view.addSubview(self.closeBtn)
+		self.closeBtn.addTarget(self,
+								action: #selector(self.closeBtnDidTap(_:)),
+								for: .touchUpInside)
+		self.closeBtn.snp.makeConstraints { (make) in
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(10)
+			make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(10)
+		}
+	}
+	
+	/// close button action
+	/// - Parameter sender: UIButton
+	@objc func closeBtnDidTap(_ sender: UIButton) {
+		self.dismiss(animated: true)
 	}
 }
